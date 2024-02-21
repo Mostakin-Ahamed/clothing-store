dd3e7c9f5e8f7083c3195410ffbb8e65082d03f6
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-02-21 20:04:34 +0600

    added footer and carousel

---
+++ src/Components/Carousel/Carouse.jsx
@@ -0,0 +1,34 @@
+
+
+const Carouse = () => {
+    return (
+        <div className="border border-red-500 h-96">
+            <div className="carousel carousel-center rounded-box">
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Carouse;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import Banner from "../../Components/Banner/Banner";
+import Carouse from "../../Components/Carousel/Carouse";
+import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>        
+            <Navbar></Navbar> 
+            <Carouse></Carouse>       
             <Banner></Banner>   
             <h2>This is homepage</h2>
+            <Footer></Footer>
         </div>
     );
 };
